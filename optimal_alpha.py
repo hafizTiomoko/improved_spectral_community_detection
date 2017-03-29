4fef55fa8d8d9d54b24f566ce6ec6811b045e2f4
Hafiz TIOMOKO ALI <tio_hafiz@yahoo.fr>
2017-03-29 11:31:25 -0400

    Update optimal_alpha.py

--- optimal_alpha.py
+++ optimal_alpha.py
@@ -9,47 +9,47 @@ def optimal_alpha(qEst,n):
 
         found_a_valid_point = 0
 
-        e2savetmp = 0
-        e2save = 1
+        gsavetmp = 0
+        gsave = 1
         val_external_watchdog = 1000
         external_watchdog = 1
 
         q_pow_1m2a = np.power(qEst, (1 - 2 * a))
         q_pow_2m2a = np.power(qEst, (2 - 2 * a))
 
-        while (abs(e2savetmp - e2save) > 1e-5 and external_watchdog < val_external_watchdog):
+        while (abs(gsavetmp - gsave) > 1e-5 and external_watchdog < val_external_watchdog):
             x = (left + right) / 2
             watchdog = 1
-            e2tmp = 0
-            e1tmp = 0
-            e1 = 1
-            e2 = 1
+            ftmp = 0
+            gtmp = 0
+            f = 1
+            g = 1
 
-            while (max([abs(e2tmp - e2), abs(e1tmp - e1)]) > 1e-10 and watchdog < 1e2):
-                e1tmp = e1
-                e2tmp= e2
+            while (max([abs(gtmp - g), abs(ftmp - f)]) > 1e-10 and watchdog < 1e2):
+                ftmp = f
+                gtmp= g
 
-                den=(1./n)/(-x - q_pow_1m2a * e1tmp + q_pow_2m2a * e2tmp)
-                e1 = np.dot(np.squeeze(np.asarray(q_pow_1m2a)),np.squeeze(np.asarray(den)))
+                den=(1./n)/(-x - q_pow_1m2a * ftmp + q_pow_2m2a * gtmp)
+                f = np.dot(np.squeeze(np.asarray(q_pow_1m2a)),np.squeeze(np.asarray(den)))
 
-                e2 = np.dot(np.squeeze(np.asarray(q_pow_2m2a)),np.squeeze(np.asarray(den)))
+                g = np.dot(np.squeeze(np.asarray(q_pow_2m2a)),np.squeeze(np.asarray(den)))
 
                 watchdog = watchdog + 1
 
             if (watchdog < 1e2):
                 found_a_valid_point = 1
-                e2savetmp = e2save
+                gsavetmp = gsave
                 right = x
-                e2save = e2
+                gsave = g
             else:
                 if (found_a_valid_point == 0):
                     right = 2 * right
                 left = x
             external_watchdog += 1
 
-        e2 = e2save
+        g = gsave
 
-        limit.append(-1.0 / e2)
+        limit.append(-1.0 / g)
 
 
     # plot(alpha,limit)
